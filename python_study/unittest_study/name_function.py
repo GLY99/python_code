2db15b5543e7e1a2582f0c334acf0e07585a60b2
gouliyang <mr_gly@163.com>
2023-01-25 15:40:50 +0800

    remove python study

--- python_study/unittest_study/name_function.py
+++
@@ -1,9 +0,0 @@
-def get_formatted_name(first: str, last: str) -> str:
-    """
-    生成整洁的名字
-    :param first: 姓
-    :param last: 名
-    :return: 格式化后的名字
-    """
-    full_name = f"{first} {last}"
-    return full_name.title()
